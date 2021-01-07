8db1a4ab32f9f7cc0a3ab731cc4b669b0bdb8843
Thea Go <theaellengo@gmail.com>
2021-01-07 12:13:38 +0800

    init commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const handlebars = require('handlebars');
+
+const app = express();
+const port = 3000;
+
+const indexRouter = require('./routes/index');
+
+app.engine(
+  'hbs',
+  exphbs({
+    extname: 'hbs',
+    defaultView: 'main',
+    layoutsDir: path.join(__dirname, '/views/layouts'),
+    partialsDir: path.join(__dirname, 'views/partials')
+  })
+);
+
+app.set('view engine', 'hbs');
+
+app.use('/', indexRouter);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
