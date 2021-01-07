84c6bae786914da631c3661930679993a68d06c1
christinedeticio <christine_deticio@dlsu.edu.ph>
2021-01-07 19:01:39 +0800

    created the controller and query hbs files

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 const router = require('express').Router();
+const controller = require('../controllers/controller.js')
 
 router.get('/', (req, res) => {
-  res.render('home', {
-    title: 'Skyflix'
-  });
+  controller.getHome(req, res);
+});
+
+router.get('/top-10-highest-grossing-movies-by-year', (req, res) => {
+  controller.getHighestGrossing(req, res);
+});
+
+router.get('/popular-movies', (req, res) => {
+  controller.getPopularMovies(req, res);
+});
+
+router.get('/total-movie-collection-earnings', (req, res) => {
+  controller.getCollectionEarnings(req, res);
+});
+
+router.get('/highest-rated-movies-by-year', (req, res) => {
+  controller.getHighestRated(req, res);
+});
+
+router.get('/top-50-similar-movies', (req, res) => {
+  controller.getSimilarMovies(req, res);
+});
+
+router.get('/top-10-most-popular-genres-by-year', (req, res) => {
+  controller.getPopularGenres(req, res);
+});
+
+router.get('/movie-info', (req, res) => {
+  controller.getMovieInfo(req, res);
 });
 
 module.exports = router;
