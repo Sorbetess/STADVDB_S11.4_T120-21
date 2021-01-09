0e9bac003e7b11ab002d18700b85dbe93eb45a11
sharmainegaw <sharmaine_gaw@dlsu.edu.ph>
2021-01-09 13:55:10 +0800

    changed hbs and added year query for options

--- controllers/queryController.js
+++ controllers/queryController.js
@@ -16,11 +16,17 @@ function isThereNextPage(queryCount, limit, currentPage)
   return (queryCount <= limit || currentPage >= lastPage) ? false : true;
 }
 
+const yearQuery =
+"SELECT DISTINCT EXTRACT(year FROM release_date) as year " + 
+"FROM Movies " +
+"WHERE release_date IS NOT NULL " +
+"ORDER BY year DESC";
+
 const pool = new Pool({
-  user: '',
+  user: 'postgres',
   host: 'localhost',
   database: 'movies',
-  password: '',
+  password: 'p@ssword',
   port: 5432
 });
 
@@ -44,23 +50,36 @@ const queryController = {
     " OFFSET " + offset;
 
     pool.query(
-      query,
-      (error, results) => {
+      yearQuery,
+      (error, years) => {
         if (error) throw error;
 
-        console.log(results.rows);
+        pool.query(
+          query,
+          (error, results) => {
+            if (error) throw error;
+    
+            console.log(results.rows);
+    
+            res.render('highest_grossing', {
+              title: "Top 10 Highest Grossing Movies in " + year,
+              movies: results.rows,
 
-        res.render('display_highest_grossing', {
-          title: "Top 10 Highest Grossing Movies in " + year,
-          movies: results.rows,
-          year: year,
-          previousPage: (currentPage - 1),
-          nextPage: parseInt(currentPage) + 1,
-          booleanPreviousPage: isTherePrevPage(currentPage),
-          booleanNextPage: isThereNextPage(results.rows[0].full_count, limit, currentPage)
-        });
+              years: years.rows,
+              
+              input_option: "year",
+              input_value: year,
+              
+              previousPage: (currentPage - 1),
+              nextPage: parseInt(currentPage) + 1,
+              booleanPreviousPage: isTherePrevPage(currentPage),
+              booleanNextPage: isThereNextPage(results.rows[0].full_count, limit, currentPage)
+            });
+          }
+        ); 
+        
       }
-    ); 
+    );
   },
   
   postMovieInfo: function (req, res) {
@@ -100,8 +119,10 @@ const queryController = {
       res.render('collection_earnings', {
         title: 'Total Movie Collection Earnings of "' + collection + '"',
         collections: results.rows,
-        collection: collection,
-        input_value: "collection",
+        
+        input_option: "collection",
+        input_value: collection,
+
         previousPage: (currentPage - 1),
         nextPage: parseInt(currentPage) + 1,
         booleanPreviousPage: isTherePrevPage(currentPage),
